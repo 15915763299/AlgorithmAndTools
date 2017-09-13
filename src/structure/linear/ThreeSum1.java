ed37b9df9731aa812040347530dafd3a74ff13c6
YuChiTao <634439570@qq.com>
2017-09-13 16:50:37 +0800

    Initial commit

---
+++ src/structure/linear/ThreeSum1.java
@@ -0,0 +1,35 @@
+package structure.linear;
+
+/**
+ * 一个数组中有没有三个数字的合为 target 所代表的数字，并输一个 boolean（找到一组即可）
+ */
+public class ThreeSum1 {
+
+    public static void main(String args[]) {
+        int[] temp = {8, -2, 1, 7, 5, 4, 6};
+        System.out.println(threeSun(temp, 5));
+    }
+
+    /**
+     * 在SumTwo的基础上加一次遍历，时间复杂度 O(n²)
+     */
+    private static boolean threeSun(int nums[], int target) {
+
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+
+        for (int num : nums) {
+            int two[] = TwoSum1.twoSum(nums, target - num);
+            if (nums[two[0]] + nums[two[1]] + num == target) {
+                System.out.println(
+                        nums[two[0]] + " + " + nums[two[1]] + " + " + num
+                                + " = " + (nums[two[0]] + nums[two[1]] + num)
+                );
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/structure/linear/TwoSum1.java
@@ -0,0 +1,43 @@
+package structure.linear;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 求一个数组中有没有两个数字的合为 target 所代表的数字，并输出这两个数字的 index（找到一组即可）
+ */
+public class TwoSum1 {
+
+    public static void main(String args[]) {
+        int[] temp = {8, -2, 1, 7, 5, 4};
+        int[] result = twoSum(temp,5);
+        System.out.println(result[0] + "," + result[1]);
+    }
+
+    /**
+     * 将 target - nums[i] 放入HashMap中，依次检查后面的数字是否等于这个值
+     * 时间复杂度：O(n)
+     */
+    static int[] twoSum(int[] nums, int target) {
+
+        int[] res = {0, 0};
+        if (nums == null || nums.length < 2) {
+            return res;
+        }
+
+        Map<Integer, Integer> remainSet = new HashMap<>();//<remain, index>
+
+        for (int i = 0; i < nums.length; i++) {
+            if (remainSet.containsKey(nums[i])) {
+                res[0] = remainSet.get(nums[i]);
+                res[1] = i;
+                return res;
+            } else {
+                remainSet.put(target - nums[i], i);
+            }
+        }
+
+        return res;
+    }
+
+}
