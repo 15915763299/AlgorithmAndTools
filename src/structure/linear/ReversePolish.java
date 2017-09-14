1ea50ce8089907dbb000737133bac1cc25c70dfb
YuChiTao <634439570@qq.com>
2017-09-14 13:22:37 +0800

    2017-09-14

--- src/structure/linear/ReversePolish.java
+++ src/structure/linear/ReversePolish.java
@@ -9,22 +9,36 @@ import java.util.LinkedList;
 public class ReversePolish {
 
 
-    public static int evalRPN(String[] tokens){
-        if(tokens == null || tokens.length == 0){
+    public static int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
             return 0;
         }
 
         Deque<Integer> stack = new LinkedList<>();//Store all numbers to be calculate
 
-        for(int i = 0; i < tokens.length; i++){
-            switch (tokens[i]){
+        for (int i = 0; i < tokens.length; i++) {
+            switch (tokens[i]) {
                 case "+":
                     stack.offerLast(stack.pollLast() + stack.pollLast());
                     break;
                 case "-":
                     int a1 = stack.pollLast();
+                    int b1 = stack.pollLast();
+                    stack.offerLast(b1 - a1);
+                    break;
+                case "*":
+                    stack.offerLast(stack.pollLast() * stack.pollLast());
+                    break;
+                case "/":
+                    int a2 = stack.pollLast();
+                    int b2 = stack.pollLast();
+                    stack.offerLast(b2 / a2);
+                    break;
+                default:
+                    stack.offerLast(Integer.parseInt(tokens[i]));
+                    break;
             }
         }
+        return stack.pollLast();
     }
-
 }
