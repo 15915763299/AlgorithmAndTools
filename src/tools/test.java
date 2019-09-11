3abeba17954ae33639b2a4b26fda6c4bfaf18ece
WeiTao <wt@enzhico.com>
2019-09-11 14:40:00 +0800

    2019-08-19

---
+++ src/tools/test.java
@@ -0,0 +1,18 @@
+//import tools.aapt.ApkInfo;
+//import tools.aapt.ApkUtil;
+//
+///**
+// * Created  on  2018/7/2 17:25
+// *
+// * @author rowliner
+// * @describe
+// */
+//public class test {
+//    public   static   void   main(String[]   args){
+//        String apkPath ="C:\\Users\\Administrator\\Desktop\\test\\PluginApp-debug-20180622_18-[V 2.1.1].apk";
+//        ApkUtil apkUtil = new ApkUtil();
+//
+//        ApkInfo apkInfo = apkUtil.parseApk(apkPath);
+//        System.out.println(apkInfo.toString());
+//    }
+//}
